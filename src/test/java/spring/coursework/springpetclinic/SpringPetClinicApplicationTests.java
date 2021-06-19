0e347fa32603421a34280210af305419e9ff4ed2
divyanshu <divyanshuanand1998@gmail.com>
2021-06-19 12:35:47 -0700

    Project Structure commit.

---
+++ src/test/java/spring/coursework/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.coursework.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
